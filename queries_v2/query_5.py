43ec66a0ba0c8784bcb817001ae8d8ea019dc85a
aashu2303 <ee20btech11060@iith.ac.in>
2023-08-06 23:34:23 +0530

    added python queries and responses

---
+++ queries_v2/query_5.py
@@ -0,0 +1,44 @@
+from elasticsearch import Elasticsearch
+import warnings
+import argparse
+
+warnings.filterwarnings("ignore")
+
+index_name = "tmp2"
+client = Elasticsearch(f"http://localhost:9200")
+
+parser = argparse.ArgumentParser(description="Generating the call graph of the user-input function in the user-input repository")
+parser.add_argument("--repo", required=True, help="Name of the repository")
+parser.add_argument("--func", required=True, help="Function Name")
+
+args = parser.parse_args()
+
+def dfs(function_name, call_graph_dict):
+    call_graph_dict[function_name] = {}
+    resp = client.search(index=index_name, pretty=True, source=["called_function_list"], size=1000, query={
+            "bool": {
+                "filter": {
+                    "match_phrase": {
+                    "File": args.repo
+                    }
+                },
+                "must": {
+                    "match_phrase": {
+                    "funcName": function_name
+                    }
+                }
+            }
+        })
+    # print(resp)
+    content = resp.raw["hits"]["hits"][0]["_source"]["called_function_list"]
+    # print(content)
+    # content = resp.raw["hits"]["hits"]
+    # print(len(content))
+
+    for func in content:
+        dfs(func, call_graph_dict=call_graph_dict[function_name])
+
+call_graph = {}
+dfs(args.func, call_graph_dict=call_graph)
+print(f"Call graph for {args.func}")
+print(call_graph)
---
+++ queries_v2/query_8.py
@@ -0,0 +1,36 @@
+from elasticsearch import Elasticsearch
+import warnings
+import argparse
+
+warnings.filterwarnings("ignore")
+
+index_name = "tmp2"
+client = Elasticsearch(f"http://localhost:9200")
+
+parser = argparse.ArgumentParser(description="Maps read by all the functions in the call graph of the user-input function")
+parser.add_argument("--func", required=True, help="Function name")
+
+args = parser.parse_args()
+
+def dfs(function_name, readMaps_list):
+
+    resp = client.search(index=index_name, pretty=True, source=["called_function_list", "readMaps"], size=1000, query={
+            "bool": {
+                "must": {
+                    "match_phrase": {
+                    "funcName": function_name
+                    }
+                }
+            }
+        }).raw["hits"]["hits"][0]["_source"]
+
+    content = resp["called_function_list"]
+    readMaps = resp['readMaps']
+    readMaps_list += readMaps
+    for func in content:
+        dfs(func, readMaps_list)
+
+READMAPS = []
+dfs(args.func, READMAPS)
+print(f"Maps read throughout the FCG of function {args.func}")
+print(READMAPS)
