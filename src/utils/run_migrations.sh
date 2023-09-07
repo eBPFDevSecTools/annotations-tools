c13262e311d9ea81a501756b53bd6f9364d0d134
Palani Kodeswaran <palani.k@gmail.com>
2023-09-07 22:59:51 +0530

    add ingress node firewall

--- src/utils/run_migrations.sh
+++ src/utils/run_migrations.sh
@@ -13,12 +13,14 @@ set -x
 
 #python3 comments_migration.py -s https://github.com/OISF/suricata.git -c 9353b07292255025069674df73b84acd88b254db -r /tmp/suricata -o ../annotations/ -p suricata -i "/Users/palani/git/ebpf-projects-annotations/projects/suricata-master/human_annotated/suricata-master.db_comments.db"
 
-python3 comments_migration.py -s https://github.com/cilium/cilium.git -c 8f606bafb696076609a01f88204d895576043950 -r /tmp/cilium -o ../annotations/ -p cilium -i "/Users/palani/git/ebpf-projects-annotations/projects/cilium/cilium_annotated.db"
+#python3 comments_migration.py -s https://github.com/cilium/cilium.git -c 8f606bafb696076609a01f88204d895576043950 -r /tmp/cilium -o ../annotations/ -p cilium -i "/Users/palani/git/ebpf-projects-annotations/projects/cilium/cilium_annotated.db"
 
 
 
-python3 comments_migration.py -s https://github.com/netobserv/netobserv-ebpf-agent.git -c e33e69016583da27507af47f4b09a9ca4538e406 -r /tmp/netobserv -o ../annotations/ -p netobserv -i "/Users/palani/git/ebpf-projects-annotations/projects/netobserv-bpf-main/human_annotated/netobserv-bpf-main.db_comments.db"
+#python3 comments_migration.py -s https://github.com/netobserv/netobserv-ebpf-agent.git -c e33e69016583da27507af47f4b09a9ca4538e406 -r /tmp/netobserv -o ../annotations/ -p netobserv -i "/Users/palani/git/ebpf-projects-annotations/projects/netobserv-bpf-main/human_annotated/netobserv-bpf-main.db_comments.db"
 
+
+python3 comments_migration.py -s https://github.com/openshift/ingress-node-firewall.git -c 7bf8d11f60420be51488fca94d9469ecb4bd855c -r /tmp/ingress-node-firewall -o ../annotations/ -p ingress-node-firewall  -i "/Users/palani/git/ebpf-projects-annotations/projects/ingress-node-firewall-master/human_annotated/ingress-node-firewall-master.db_comments.db"
 
 
 
